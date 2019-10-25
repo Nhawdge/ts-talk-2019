b382dd5a8176407d1f0ca4504ac3ff842d73c4e9
John Pavek <john.pavek@blendinteractive.com>
2019-10-24 22:49:50 -0500

    Slide 5 Clean up and comments!

--- mileagetracker/classes/FormField.ts
+++ mileagetracker/classes/FormField.ts
@@ -1,3 +1,5 @@
+//TODO Part 1
+// Abstraction distraction!
 abstract class FormField {
     Name: string;
     Type: string;
@@ -8,6 +10,8 @@ abstract class FormField {
      * This is form field  
      */
     constructor(input: any) {
+        // TODO Part 2
+        // Input Any was probably a bad idea here.
         this.Name = input.Name;
         this.Type = input.Type;
         this.Value = input.Value || 0;
@@ -20,7 +24,6 @@ abstract class FormField {
         labelElem.innerHTML = this.Name;
 
         let inputElem = document.createElement('input');
-        //inputElem.type = this.Type;
         inputElem.name = this.Name;
         if (this.Value) {
             inputElem.value = this.Value;
@@ -28,22 +31,26 @@ abstract class FormField {
 
         this.Attributes.forEach(x => inputElem.setAttribute(x[0], x[1]));
 
-        //labelElem.innerText = input;
         group.appendChild(labelElem)
         group.appendChild(inputElem)
         return group;
     }
 }
 
-
+// TODO Part 3
+// Typescript extends? Tell me more!
 class NumberFormField extends FormField {
     /**
      * Form field for number type
      */
+    // TODO Part 4
+    // !!!Constructor danger!!!!
     constructor(name: string, placeholder: string = "", value: number = 0, attributes: Array<[string, string]> = new Array<[string, string]>()) {
         if (placeholder) {
             attributes.push(["placeholder", placeholder])
         }
+        // TODO Part 5
+        // Did you just...? Wow...
         var properties = {
             Name: name,
             Type: "number",
@@ -54,6 +61,9 @@ class NumberFormField extends FormField {
     }
 }
 
+
+// TODO Part 6
+// Again from the top!
 class DateFormField extends FormField {
     /**
      *Form field for Date types
